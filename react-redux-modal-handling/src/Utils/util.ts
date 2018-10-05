66b24d97f06b7e2eb1c8120b7047741f6c73d2aa
Felipe Vaz <felipevnunes@gmail.com>
2018-10-04 20:06:33 -0500

    te

--- react-redux-modal-handling/src/Utils/util.ts
+++ react-redux-modal-handling/src/Utils/util.ts
@@ -1,5 +1,5 @@
 export const generateKey = (): string => {
     const time = (new Date()).getTime();
     const key = Math.ceil(time * Math.random());
-    return key.toString();//c
+    return key.toString();//d
 }
